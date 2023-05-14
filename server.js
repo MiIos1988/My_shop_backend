e3e62675b49dfaedfce0327c3caaf74f1010a243
Milos1988 <vojvoda1988@gmail.com>
2023-05-14 22:22:23 +0200

    Move cors

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ mongoose
   .then((data) => console.log("MongoDB is connecting..."))
   .catch((error) => console.log(error));
 
+  app.use(cors());
 app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
-app.use(cors());
 
 app.use("/api/auth", authRoute);
 app.use("/api/product", productRoute);
